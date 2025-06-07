339393983e3b19bdaea34e34db5530b6b0e4efb1
Liforra-cloud <info.liforra@gmail.com>
2025-06-08 00:02:38 +0200

    Update tailwind.config.js

--- najem-app/tailwind.config.js
+++ najem-app/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',  // ← DŮLEŽITÉ
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+content: [
+  './src/app/**/*.{js,ts,jsx,tsx}',
+  './src/components/**/*.{js,ts,jsx,tsx}',
+]
   theme: {
     extend: {},
   },
