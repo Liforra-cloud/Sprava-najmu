adc2c5b86483d7a0c6b6a2a9c621b77c17f52ddc
Liforra-cloud <info.liforra@gmail.com>
2025-06-09 16:02:53 +0200

    Create layout.tsx

---
+++ najem-app/app/reset-password/layout.tsx
@@ -0,0 +1,7 @@
+// app/reset-password/layout.tsx
+
+import { ReactNode } from 'react';
+
+export default function ResetPasswordLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
