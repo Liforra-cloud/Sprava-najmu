6ecd6808090aabb72f6af5b6f6c9b5448d5c897d
Liforra-cloud <info.liforra@gmail.com>
2025-06-09 16:44:25 +0200

    Update layout.tsx

--- najem-app/app/(public)/layout.tsx
+++ najem-app/app/(public)/layout.tsx
@@ -1,7 +1,12 @@
-// najem-app/app/(public)/layout.tsx
+// app/(public)/layout.tsx
 
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <h1 style={{color: 'green', background: 'yellow'}}>PUBLIC LAYOUT</h1>
+      {children}
+    </>
+  )
 }
