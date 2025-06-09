a1ded8a5ec9b8fc87370dc7a01f41431e5c0086f
Liforra-cloud <info.liforra@gmail.com>
2025-06-09 17:22:55 +0200

    Create page.tsx

---
+++ najem-app/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// app/dashboard/page.tsx
+
+export default function DashboardPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-center">
+        Probíhá výstavba 🚧
+      </h1>
+    </main>
+  );
+}
