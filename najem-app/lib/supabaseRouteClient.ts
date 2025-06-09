934b821faca038bec371f62c0eafbe5caa152328
Liforra-cloud <info.liforra@gmail.com>
2025-06-09 14:26:24 +0200

    Create supabaseRouteClient.ts

---
+++ najem-app/lib/supabaseRouteClient.ts
@@ -0,0 +1,7 @@
+import { cookies } from 'next/headers'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { type Database } from '@/types/supabase'
+
+export const supabaseRouteClient = () => {
+  return createRouteHandlerClient<Database>({ cookies })
+}
