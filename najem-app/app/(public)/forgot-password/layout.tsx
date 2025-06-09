e5579f214efbea8b49ae1d38f8012179ac33345c
Liforra-cloud <info.liforra@gmail.com>
2025-06-09 16:38:22 +0200

    Delete najem-app/app/(public)/forgot-password/layout.tsx

--- najem-app/app/(public)/forgot-password/layout.tsx
+++
@@ -1,7 +0,0 @@
-// app/forgot-password/layout.tsx
-
-import { ReactNode } from 'react';
-
-export default function ForgotPasswordLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
