395ff486e1c5ad01815875459cb16d7425032bf5
Liforra-cloud <info.liforra@gmail.com>
2025-06-09 17:02:49 +0200

    Delete najem-app/app/test directory

--- najem-app/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-50 text-xl text-blue-900">
-      <div className="p-6 bg-white rounded shadow">
-        ✅ Tailwind funguje správně!
-      </div>
-    </div>
-  )
-}
