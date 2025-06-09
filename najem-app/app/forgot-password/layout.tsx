ce7350c5a08df4f5e9581b10d17fd39f1f193043
Liforra-cloud <info.liforra@gmail.com>
2025-06-09 16:02:30 +0200

    Create layout.tsx

---
+++ najem-app/app/forgot-password/layout.tsx
@@ -0,0 +1,7 @@
+// app/forgot-password/layout.tsx
+
+import { ReactNode } from 'react';
+
+export default function ForgotPasswordLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
