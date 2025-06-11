7d20894c20dc2be92dd220f14b701480d99a81d5
Liforra-cloud <info.liforra@gmail.com>
2025-06-11 21:57:25 +0200

    Create layout.tsx

---
+++ najem-app/app/expenses/layout.tsx
@@ -0,0 +1,18 @@
+// app/expenses/layout.tsx
+
+import { ReactNode } from 'react'
+import SidebarLayout from '@/components/SidebarLayout'
+import { redirect } from 'next/navigation'
+import { supabaseServerClient } from '@/lib/supabaseServerClient'
+
+export default async function TenantsLayout({ children }: { children: ReactNode }) {
+  const supabase = supabaseServerClient()
+  const { data: { user } } = await supabase.auth.getUser()
+  if (!user) redirect('/login')
+
+  return (
+    <SidebarLayout>
+      {children}
+    </SidebarLayout>
+  )
+}
