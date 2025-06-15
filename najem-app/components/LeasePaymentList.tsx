b988200420e343c3003fdcb44a3caf4049a2b655
Liforra-cloud <info.liforra@gmail.com>
2025-06-15 13:22:26 +0200

    Update LeasePaymentList.tsx

--- najem-app/components/LeasePaymentList.tsx
+++ najem-app/components/LeasePaymentList.tsx
@@ -10,11 +10,12 @@ type Props = {
 export default function LeasePaymentList({ leaseId }: Props) {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    alert('Tady to funguje!');
+    alert('Formulář odeslán pro leaseId: ' + leaseId);
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <h2>Lease ID: {leaseId}</h2>
       <button type="submit">Test Submit</button>
     </form>
   );
